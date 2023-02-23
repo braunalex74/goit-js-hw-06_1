201662fc655c58e86d0564a9e8447bece7ec8ea5
braunalex74 <braunalex74@googlemail.com>
2023-02-23 21:20:49 +0200

    DZ_6_1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,13 @@
-const navEl = document.querySelector("ul");
-console.log(navEl);
+// const listItemEl = document.querySelectorAll(".item");
+// console.log(`Number of item: ${listItemEl}`);
+
+// const navTitleEl = document.querySelector(".item");
+// const firstTitleEl = navTitleEl.firstElementChild.textContent;
+// const lastTitleEl = navTitleEl.lastElementChild;
+
+// categories.forEach((el) => {
+//   console.log(`Number of categories: ${el.firstElementChild.textContent}`);
+// });
+
+const listEl = document.querySelector(".item");
+console.log(listEl);
