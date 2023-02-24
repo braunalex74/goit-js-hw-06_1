759b8a54c036e9e44c4eac5a9d80e54a61264bc8
braunalex74 <braunalex74@googlemail.com>
2023-02-24 20:29:24 +0200

    DZ_6_1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,16 +1,10 @@
-// const listItemEl = document.querySelectorAll(".item");
-// console.log(`Number of item: ${listItemEl}`);
+const listItemEl = document.querySelectorAll(".item");
+console.log(`Number of item: ${listItemEl}`);
 
-// const navTitleEl = document.querySelector(".item");
-// const firstTitleEl = navTitleEl.firstElementChild.textContent;
-// const lastTitleEl = navTitleEl.lastElementChild;
+const navTitleEl = document.querySelector(".item");
+const firstTitleEl = navTitleEl.firstElementChild.textContent;
+const lastTitleEl = navTitleEl.lastElementChild;
 
-// categories.forEach((el) => {
-//   console.log(`Number of categories: ${el.firstElementChild.textContent}`);
-// });
-
-const listEl = document.querySelector(".item");
-console.log(listEl);
-
-const quantityItemEl = document.querySelectorAll(".item");
-console.log(quantityItemEl);
+categories.forEach((el) => {
+  console.log(`Number of categories: ${el.firstElementChild.textContent}`);
+});
